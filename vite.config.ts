24a17391392003d708bb451d44888bdc97986d2f
David Silva <51048697+jxtzzz@users.noreply.github.com>
2026-02-18 22:43:35 +0000

    fixing vite config

--- vite.config.ts
+++ vite.config.ts
@@ -35,11 +35,5 @@ export default defineConfig(({ mode }) => ({
         },
       },
     },
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
   },
 }));
